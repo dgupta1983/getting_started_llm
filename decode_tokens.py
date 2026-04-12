17298a315aacd1c40bf2db531d884d9bab9a5967
dgupta1983 <deepak@newagecoders.com>
2026-04-12 12:19:10 -0700

    token degrade

--- decode_tokens.py
+++ decode_tokens.py
@@ -1,12 +1,17 @@
 from transformers import AutoTokenizer
 
-prompt = "It was dark and stormy"
+prompt = "Hope this is not bringing asparagusohikdjjds in too many tokens as this is a very long sentence"
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2-0.5B")
 input_ids = tokenizer(prompt).input_ids
 
 input_ids 
-
+print("\t : ",input_ids)
+token_count = 0
 for t in input_ids:
     print("\t : ",tokenizer.decode(t))
+    token_count+=1
+print("Number of words in the prompt : ",len(prompt.split()))
+print("Number of tokens in the prompt : ",token_count)
+    
     
 
